3be207635cf5ec623497f6e604bcf3d0197ae3b5
Kanupriya30 <Anjalikkanu@gmail.com>
2026-08-18 01:57:54 +0530

    Complete student management database

--- student_management/student_management.sql
+++ student_management/student_management.sql
@@ -1,5 +1,7 @@
 create database student_management;
 use student_management;
+
+
 create table students(
     name varchar(50),
     student_id int primary key,
@@ -18,33 +20,132 @@ VALUES
 (1008, 'Ishaan', 'AIDS', 20),
 (1009, 'Ananya', 'CE', 21),
 (1010, 'Rohan', 'CSE', 19);
-SELECT * FROM students;
-
-SELECT name
-FROM students
-WHERE course= 'CSE';
-
-SELECT name
-FROM students
-WHERE course = 'CSE' AND year = 21;
-
-SELECT name
-FROM students
-WHERE course = 'CSE' OR course = 'AIDS';
-
-SELECT name
-FROM students
-WHERE year > 19;
 
 
-SELECT * From students
-ORDER BY year DESC;
 
-SELECT * From students
-ORDER BY year ASC;
 
-SELECT * from students
-ORDER BY course, name asc;
 
-SELECT name, course, year from students
-ORDER BY year desc;
+
+
+create table marks(
+mark_id int primary key,
+student_id int,
+subject varchar(50),
+marks int,
+FOREIGN KEY (student_id)
+REFERENCES students(student_id)
+ );
+INSERT INTO marks (mark_id, student_id, subject, marks)
+VALUES
+(1, 1001, 'DBMS', 88),
+(2, 1001, 'Python', 92),
+(3, 1001, 'CS', 81),
+(4, 1001, 'DSA', 62),
+(5, 1002, 'DBMS', 81),
+(6, 1002, 'Python', 85),
+(7, 1002, 'CS', 45),
+(8, 1002, 'DSA', 100),
+(9, 1003, 'DBMS', 76),
+(10, 1003, 'CS', 33),
+(11, 1003, 'DSA', 71),
+(12, 1003, 'Python', 100);
+INSERT INTO marks (mark_id, student_id, subject, marks)
+VALUES
+(13, 1004, 'DBMS', 78),
+(14, 1004, 'Python', 84),
+(15, 1004, 'CS', 72),
+(16, 1004, 'DSA', 69),
+
+(17, 1005, 'DBMS', 91),
+(18, 1005, 'Python', 87),
+(19, 1005, 'CS', 79),
+(20, 1005, 'DSA', 88),
+
+(21, 1006, 'DBMS', 74),
+(22, 1006, 'Python', 81),
+(23, 1006, 'CS', 68),
+(24, 1006, 'DSA', 77),
+
+(25, 1007, 'DBMS', 85),
+(26, 1007, 'Python', 90),
+(27, 1007, 'CS', 82),
+(28, 1007, 'DSA', 94),
+
+(29, 1008, 'DBMS', 88),
+(30, 1008, 'Python', 76),
+(31, 1008, 'CS', 84),
+(32, 1008, 'DSA', 80),
+
+(33, 1009, 'DBMS', 71),
+(34, 1009, 'Python', 79),
+(35, 1009, 'CS', 73),
+(36, 1009, 'DSA', 68),
+
+(37, 1010, 'DBMS', 93),
+(38, 1010, 'Python', 89),
+(39, 1010, 'CS', 91),
+(40, 1010, 'DSA', 95);
+
+
+
+
+#-----------joins--------------------
+
+select name, course,marks,subject from students
+inner join marks
+on marks.student_id= students.student_id;
+
+#------------avg()marks-----------------------
+select name ,avg( marks) from students  
+inner join marks  on marks.student_id =students.student_id
+group by name;
+
+#---------------name of student with highest marks--------
+select name , max(marks) as highest_marks from students 
+inner join marks on marks.student_id=students.student_id
+group by name;
+#-----------avergae marks in course--------------
+select course, avg(marks) from students 
+inner join marks on marks.student_id=students.student_id
+group by course;
+
+#------------having query --------
+select course, avg(marks) from students 
+inner join marks on marks.student_id=students.student_id
+group by course
+having avg(marks)>80;
+
+#--------------student performance report--------# 
+
+select name as student, sum(marks) as total_marks , avg(marks) as Average_marks from students 
+inner join marks on marks.student_id=students.student_id
+group by name;
+ #---------------top performing students-------------#
+ select name as student, sum(marks) as total_marks , avg(marks) as Average_marks from students 
+inner join marks on marks.student_id=students.student_id
+group by name
+having avg(marks)>80;
+#--------------asec desc report ----------------------#
+select name as student, sum(marks) as total_marks , avg(marks) as Average_marks from students 
+inner join marks on marks.student_id=students.student_id
+group by name
+order by avg(marks) desc;
+
+#-----------------subject-wise performance------------#
+select   subject, avg(marks)as Avergae_marks from students
+inner join marks on marks.student_id=students.student_id
+group by subject;
+
+#-----------students who failed in any subject------------
+select name as student, subject ,marks from students 
+inner join marks on marks.student_id=students.student_id
+where marks <40;
+
+#-----------student with highest avg report--------------#
+select name as student, avg(marks) from students 
+inner join marks on marks.student_id=students.student_id
+group by name 
+order by avg(marks) desc limit 1;
+
+
+
